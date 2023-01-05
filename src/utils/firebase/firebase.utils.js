c709956aa56c5612065a37b2d0151d290533e2f1
MichaelXerxes <hiszpan4444@googlemail.com>
2023-01-05 18:49:33 +0000

    firebase cdn

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,11 +1,10 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import {getAuth,signInWithRedirect,
+        signInWithPopup,GoogleAuthProvider,} from 'firebase/auth';
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+
 const firebaseConfig = {
   apiKey: "AIzaSyD1Cslwd4B9mNkiIICGY1OLtlEF9WqyHWU",
   authDomain: "e-shop-tamplate-new.firebaseapp.com",
@@ -16,6 +15,13 @@ const firebaseConfig = {
   measurementId: "G-N8PY26CRZF"
 };
 
-// Initialize Firebase
+
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+
+const provider =new GoogleAuthProvider();
+provider.setCustomParameters({
+    prompt:"select_account"
+});
+export const auth=getAuth();
+export const singInWithPopIpGoogle=()=> signInWithPopup(auth,provider);
