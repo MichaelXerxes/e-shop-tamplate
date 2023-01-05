b17564ee1d2c670be17d2956c026b01652a79760
MichaelXerxes <hiszpan4444@googlemail.com>
2023-01-05 19:03:15 +0000

    firebase cdn

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,8 +1,14 @@
+import {singInWithPopIpGoogle} from '../../utils/firebase/firebase.utils';
 const SignIn=()=>{
+    const logoGoogleUser=async ()=>{
+        const response =await singInWithPopIpGoogle();
+    }
     return (
         <div>
             <h1>Sign In</h1>
-            <h1>Sign In</h1>
+           <button  onClick={logoGoogleUser}>
+            Sign In with PopUp Google
+            </button>
             <h1>Sign In</h1>
         </div>
     );
