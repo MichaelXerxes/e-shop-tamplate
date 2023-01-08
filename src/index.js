1ac7357f52d24899c7fbe9ae8c47ef9cd5009d4e
MichaelXerxes <hiszpan4444@googlemail.com>
2023-01-08 19:11:16 +0000

    products context

--- src/index.js
+++ src/index.js
@@ -1,23 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import { UserProvider } from './contexts/user.context';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import Footer from './components/footer/footer.component';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { UserProvider } from "./contexts/user.context";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import Footer from "./components/footer/footer.component";
+import { ProductProvider } from "./contexts/product.context";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-    <UserProvider>
-    <App />
-    </UserProvider>
-    
-    <Footer/>
+      <UserProvider>
+        <ProductProvider>
+        <App />
+        </ProductProvider>
+      </UserProvider>
+      <Footer />
     </BrowserRouter>
-    
   </React.StrictMode>
 );
 
--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,24 +1,16 @@
 import './shop.styles.scss';
 import SHOP_DATA from '../../shop-data.json';
 import ProductCard from '../../components/product-card/product-card.component';
+import { useContext } from 'react';
+import { ProductsContext } from '../../contexts/product.context';
 
 const Shop=()=>{
+  const {products}=useContext(ProductsContext)
     return (
-      <div>
-        {SHOP_DATA.map(({id,name,imageUrl,price})=>(
-         <div className='category-container'>
-         <div
-           className='background-image'
-           style={{
-             backgroundImage: `url(${imageUrl})`,
-           }}
-         />
-         <div className='category-body-container'>
-           <h4>Price: {price}</h4>
-           <p>Buy Now</p>
-         </div>
-       </div>
-        ))};
+      <div className='products-container'>
+        { SHOP_DATA.map((product)=>(
+    <ProductCard key={product.id} product={product} />
+   ))};
       </div>
     );
   }
